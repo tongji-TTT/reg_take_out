33642a8907a32a6fc01866270188e374ea213b6d
20133 <2013332749@qq.com>
2024-01-03 20:43:54 +0800

    windows

--- src/main/java/demo/controller/CategoryController.java
+++ src/main/java/demo/controller/CategoryController.java
@@ -1,5 +1,7 @@
 package demo.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import demo.common.R;
 import demo.entity.Category;
 import demo.serivce.CategoryService;
@@ -22,8 +24,34 @@ public class CategoryController {
 
     @PostMapping()
     public R<String> save(@RequestBody Category category){
-        log.info("{}",category);
+        //log.info("{}",category);
         categoryService.save(category);
         return R.success("新增分类成功");
     }
+
+//    @GetMapping("/page")
+//    public R<Page> page(int page ,int pageSize){
+//        //分页器
+//        Page<Category> categoryPage = new Page<>(page,pageSize);
+//        //条件构造器
+//        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+//        queryWrapper.orderByAsc(Category::getSort);
+//        categoryService.page(categoryPage,queryWrapper);
+//        return R.success(categoryPage);
+//
+//    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize){
+
+        //创建一个分页构造器
+        Page categoryPage = new Page<>(page,pageSize);
+        //创建一个条件构造器  用来排序用的  注意这个条件构造器一定要使用泛型，否则使用条件查询这个方法的时候会报错
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper();
+        //添加排序条件 ，根据sort字段进行排序
+        queryWrapper.orderByAsc(Category::getSort);
+        categoryService.page(categoryPage,queryWrapper);
+        return R.success(categoryPage);
+    }
+
 }
